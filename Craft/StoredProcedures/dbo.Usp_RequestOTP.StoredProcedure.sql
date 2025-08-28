aab80eb7b6fccc151d4b64166d14e167b02e5d1b
Rameshkumar Krishnasamy <ramesh0505rk@gmail.com>
2025-08-28 10:24:17 +0530

    Implemented Usp_RequestOTP SP.

---
+++ Craft/StoredProcedures/dbo.Usp_RequestOTP.StoredProcedure.sql
@@ -0,0 +1,45 @@
+USE [Craft]
+GO
+/****** Object:  StoredProcedure [dbo].[Usp_RequestOTP]    Script Date: 28-08-2025 09:59:27 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Rameshkumar K
+-- Create date: 27/08/2025
+-- Description:	Created to generate OTP for password reset.
+-- =============================================
+-- =============================================
+-- Changes field
+-- Date of change			Name						Description
+-- 26/08/2025				Rameshkumar Krishnasamy		Created
+-- =============================================
+CREATE OR ALTER   PROCEDURE [dbo].[Usp_RequestOTP]
+	@UserEmail NVARCHAR(300),
+	@Otp INT OUTPUT
+AS
+BEGIN
+--------------------------------------------------------
+-- Declare local variables
+--------------------------------------------------------
+	DECLARE @LocalUserEmail NVARCHAR(300) = @UserEmail,
+			@UserID UNIQUEIDENTIFIER
+--------------------------------------------------------
+	
+	-- Check if user exists
+	SELECT @UserID = UserID FROM Users WHERE UserEmail = @LocalUserEmail
+
+	IF @UserID IS NULL
+	BEGIN
+		-- Set OTP = 0, if the user doesn't exist
+		SET @Otp = 0 
+		RETURN
+	END
+
+	-- Generate 4 digit OTP
+	SET @Otp = FLOOR(RAND() * 9000) + 1000
+
+	INSERT INTO PasswordResetOtps (UserID, Otp, ExpiryDate, IsUsed) VALUES
+	(@UserID, @Otp, DATEADD(MINUTE,10,GETUTCDATE()), 0)
+END
